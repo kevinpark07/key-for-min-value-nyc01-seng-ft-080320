698730adf9a74ccb0d2715cb77793b3d53a59eeb
Kevin Park <kevin.park07@yahoo.com>
2020-07-12 22:00:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,11 @@
 def key_for_min_value(name_hash)
 min_key = ""
 min_value = nil
-name_hash.each {|key, value|
+name_hash.each do |key, value|
 if min_value == nil || value < min_value
   min_value = value
   min_key = key
 end
-}
+end
 min_key
 end
