9e8cadd6d29e789a31b05245d27fa304072e0966
Kevin Park <kevin.park07@yahoo.com>
2020-07-12 21:49:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+min_key = ""
 min_value = nil
 name_hash.each {|key, value|
 if min_value == nil 
